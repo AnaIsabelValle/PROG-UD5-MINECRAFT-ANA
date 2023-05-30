d91419c213e226afb883814d69f66e292c7f5e68
AnaIsabelValle <usr3048@salesianos-lacuesta.net>
2023-05-30 12:51:03 +0100

    fix(Personaje) El método recibir ataqyue, debe mostrar el daño recibido y la salud actual

--- src/com/minecraft/minecraft_ana/Personaje.java
+++ src/com/minecraft/minecraft_ana/Personaje.java
@@ -20,6 +20,12 @@ public class Personaje implements Combate {
 
     @Override
     public void recibirAtaque(int ataque) {
-
+        int dañoRecibido = ataque - defensa;
+        if (dañoRecibido < 0) {
+            dañoRecibido = 0;
+        }
+        salud -= dañoRecibido;
+        System.out.println("El personaje ha recibido un daño de: " + dañoRecibido);
+        System.out.println("El personaje tiene una salud de : " + salud);
     }
 }
