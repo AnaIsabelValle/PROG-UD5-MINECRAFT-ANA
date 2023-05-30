1633956b75ce2697b7bbd9e1f0a024adb6874e6e
AnaIsabelValle <usr3048@salesianos-lacuesta.net>
2023-05-30 11:20:03 +0100

    feat:(Personaje) Creación; De la clase, atributos y constructor

---
+++ src/com/minecraft/minecraft_ana/Personaje.java
@@ -0,0 +1,13 @@
+package com.minecraft.minecraft_ana;
+
+public class Personaje {
+    private int salud;
+    private int defensa;
+    private int fuerza;
+
+    public Personaje(int defensa, int fuerza) {
+        this.salud = 12;
+        this.defensa = defensa;
+        this.fuerza = fuerza;
+    }
+}
