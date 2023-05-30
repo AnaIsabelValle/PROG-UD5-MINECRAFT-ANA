864e72d2b650ab6c195b36d633479b5733b15868
AnaIsabelValle <usr3048@salesianos-lacuesta.net>
2023-05-30 11:34:10 +0100

    fix:(Mob) Creación; De la clase abstrasta, atributo y método abstracto

--- src/com/minecraft/minecraft_ana/Mob.java
+++ src/com/minecraft/minecraft_ana/Mob.java
@@ -1,4 +1,10 @@
 package com.minecraft.minecraft_ana;
 
 public abstract class Mob {
+    private int salud;
+
+    public Mob(int salud) {
+        this.salud = salud;
+    }
+    public abstract void moverse();
 }
